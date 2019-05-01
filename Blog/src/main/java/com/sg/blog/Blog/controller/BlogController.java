bb2d486756252137ae01a78fdb1975709511a915
augas26 <49340097+augas26@users.noreply.github.com>
2019-05-01 11:01:47 -0500

    updated

--- Blog/src/main/java/com/sg/blog/Blog/controller/BlogController.java
+++ Blog/src/main/java/com/sg/blog/Blog/controller/BlogController.java
@@ -11,10 +11,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-/**
- *
- * @author kahin
- */
+
 @Controller
 public class BlogController {
 
