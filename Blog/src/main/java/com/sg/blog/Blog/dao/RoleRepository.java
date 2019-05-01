380fdf298ef1ae5f197831b425e8fcd461a708d2
augas26 <49340097+augas26@users.noreply.github.com>
2019-05-01 11:04:05 -0500

    Updated

--- Blog/src/main/java/com/sg/blog/Blog/dao/RoleRepository.java
+++ Blog/src/main/java/com/sg/blog/Blog/dao/RoleRepository.java
@@ -4,10 +4,7 @@ import com.sg.blog.Blog.entity.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-/**
- *
- * @author Stuart
- */
+
 @Repository
 public interface RoleRepository extends JpaRepository<Role, Integer>{
 
