7b28e6ce7dcdaaffbab1ec0ae874ae91d05947e0
augas26 <49340097+augas26@users.noreply.github.com>
2019-05-01 11:03:44 -0500

    Updated

--- Blog/src/main/java/com/sg/blog/Blog/dao/PostRepository.java
+++ Blog/src/main/java/com/sg/blog/Blog/dao/PostRepository.java
@@ -7,10 +7,6 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-/**
- *
- * @author Stuart
- */
 @Repository
 public interface PostRepository extends JpaRepository<Post, Integer>{
     public List<Post> findByApprovedFalse();
