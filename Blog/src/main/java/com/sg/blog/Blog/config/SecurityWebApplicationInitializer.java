7ed726e8b23d91a0df83baaeb6c6565895d1b4cb
augas26 <49340097+augas26@users.noreply.github.com>
2019-05-01 11:00:59 -0500

    updated

--- Blog/src/main/java/com/sg/blog/Blog/config/SecurityWebApplicationInitializer.java
+++ Blog/src/main/java/com/sg/blog/Blog/config/SecurityWebApplicationInitializer.java
@@ -2,10 +2,7 @@ package com.sg.blog.Blog.config;
 
 import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
 
-/**
- *
- * @author Stuart
- */
+
 public class SecurityWebApplicationInitializer
         extends AbstractSecurityWebApplicationInitializer {
 
