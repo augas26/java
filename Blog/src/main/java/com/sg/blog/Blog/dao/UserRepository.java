b0aa6bf369b7f2e5f71b481c4f6280073e4cc14b
augas26 <49340097+augas26@users.noreply.github.com>
2019-05-01 11:04:50 -0500

    Updated

--- Blog/src/main/java/com/sg/blog/Blog/dao/UserRepository.java
+++ Blog/src/main/java/com/sg/blog/Blog/dao/UserRepository.java
@@ -5,10 +5,7 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-/**
- *
- * @author Stuart
- */
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer>{
     List<User> findByUsername(String username);
