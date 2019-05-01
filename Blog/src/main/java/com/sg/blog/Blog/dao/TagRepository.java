db34971608c05473d6a624d8f22c5c608d654511
augas26 <49340097+augas26@users.noreply.github.com>
2019-05-01 11:04:26 -0500

    Updated

--- Blog/src/main/java/com/sg/blog/Blog/dao/TagRepository.java
+++ Blog/src/main/java/com/sg/blog/Blog/dao/TagRepository.java
@@ -4,10 +4,7 @@ import com.sg.blog.Blog.entity.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-/**
- *
- * @author Stuart
- */
+
 @Repository
 public interface TagRepository extends JpaRepository<Tag, String>{
 
